3b1c93726e92bbb55b17948aee8a2a5fbaa9ab80
Mahesh <maheshmarpudi11@gmail.com>
2021-12-10 00:16:10 +0530

    gs-wep app

---
+++ gs-web/src/main/java/com/gs/web/GsWebApplication.java
@@ -0,0 +1,13 @@
+package com.gs.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsWebApplication.class, args);
+	}
+
+}
