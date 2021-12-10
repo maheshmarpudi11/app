3ab8ec4915e1fb0505473ed75aeee75d09163085
Mahesh <maheshmarpudi11@gmail.com>
2021-12-10 19:19:07 +0530

    files are modified

--- gs-web/src/main/java/com/gs/web/GsWebApplication.java
+++ gs-web/src/main/java/com/gs/web/GsWebApplication.java
@@ -1,8 +1,6 @@
 package com.gs.web;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
